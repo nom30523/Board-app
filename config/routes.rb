3db9322c6446180076c7af254430d7d4c281e9b3
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-18 21:51:35 +0900

    comments controller create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'boards#index'
-  resources :boards, except: [:index]
+  resources :boards, except: %i[index]
+  resources :comments, only: %i[create destroy]
 end
