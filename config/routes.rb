bff29c42892a1bb9024e835663b765032373a7e8
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-19 21:36:59 +0900

    user session モデル&コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/me'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'boards#index'
   resources :boards, except: %i[index]
   resources :comments, only: %i[create destroy]
