7fc8c0f2bdcd85496fc0112e761bcc5d463e223a
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-20 18:41:16 +0900

    login機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,18 @@
 class SessionsController < ApplicationController
+  def new; end
+
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   root 'boards#index'
   resources :users, only: %i[new create]
+  resources :sessions, only: %i[new]
   resources :boards, except: %i[index]
   resources :comments, only: %i[create destroy]
 end
